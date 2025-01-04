6a503cfd4425d6c853bb3969ed0e446bf9c938dc
Caroline Cahilly <caroline.cahilly@gmail.com>
2025-01-04 12:17:06 -0700

    evaluating possible dice to keep

--- gameConstants.py
+++ gameConstants.py
@@ -2,43 +2,43 @@ NUM_DICE = 6
 MIN_PLAYERS = 2
 WIN_THRESHOLD = 10000
 FIRST_TURN_THRESHOLD = 500
-POINT_SCORING_COMBINATIONS = {[1]: 100, 
-                              [5]: 50, 
-                              [1, 1, 1]: 300, 
-                              [2, 2, 2]: 200, 
-                              [3, 3, 3]: 300, 
-                              [4, 4, 4]: 400, 
-                              [5, 5, 5]: 500, 
-                              [6, 6, 6]: 600, 
-                              [1, 2, 3, 4, 5, 6]: 1500,
+POINT_SCORING_COMBINATIONS = {(1,): 100, 
+                              (5,): 50, 
+                              (1, 1, 1): 300, 
+                              (2, 2, 2): 200, 
+                              (3, 3, 3): 300, 
+                              (4, 4, 4): 400, 
+                              (5, 5, 5): 500, 
+                              (6, 6, 6): 600, 
+                              (1, 2, 3, 4, 5, 6): 1500,
                               }
 
 # Four of a kind 
 for i in range(1, NUM_DICE + 1):
-    POINT_SCORING_COMBINATIONS[[i, i, i, i]] = 1000
+    POINT_SCORING_COMBINATIONS[(i, i, i, i)] = 1000
 
 # Five of a kind
 for i in range(1, NUM_DICE + 1):
-    POINT_SCORING_COMBINATIONS[[i, i, i, i, i]] = 2000
+    POINT_SCORING_COMBINATIONS[(i, i, i, i, i)] = 2000
 
 # Six of a kind
 for i in range(1, NUM_DICE + 1):
-    POINT_SCORING_COMBINATIONS[[i, i, i, i, i, i]] = 3000
+    POINT_SCORING_COMBINATIONS[(i, i, i, i, i, i)] = 3000
 
 # Three pairs
 for i in range(1, NUM_DICE + 1):
     for j in range(i + 1, NUM_DICE + 1):
         for k in range(j + 1, NUM_DICE + 1):
-            POINT_SCORING_COMBINATIONS[[i, i, j, j, k, k]] = 1500
+            POINT_SCORING_COMBINATIONS[(i, i, j, j, k, k)] = 1500
 
 # Four of a kind and a pair
 for i in range(1, NUM_DICE + 1):
     for j in range(1, NUM_DICE + 1):
         if i != j:
-            roll = [i, i, i, i, j, j].sort()
+            roll = tuple([i, i, i, i, j, j].sort())
             POINT_SCORING_COMBINATIONS[roll] = 1500
 
 # Two triples
 for i in range(1, NUM_DICE + 1):
     for j in range(i + 1, NUM_DICE + 1):
-        POINT_SCORING_COMBINATIONS[[i, i, i, j, j, j]] = 2500
+        POINT_SCORING_COMBINATIONS[(i, i, i, j, j, j)] = 2500
--- players.py
+++ players.py
@@ -1,4 +1,4 @@
-from gameConstants import NUM_DICE
+from gameConstants import NUM_DICE, POINT_SCORING_COMBINATIONS
 import random
 
 class Player:
@@ -11,16 +11,36 @@ class Player:
         raise NotImplementedError("Subclasses must implement this method")
     
     def roll_dice(self):
-        roll = []
+        roll = {}
         
         # If all dice are kept, reset the kept dice count
         if self.dice_kept == NUM_DICE:
             self.dice_kept = 0
 
         for _ in range(NUM_DICE - self.dice_kept):
-            roll.append(random.randint(1, 6))
+            roll.add(random.randint(1, 6))
         
-        return roll.sort()
+        return roll
     
+    # Roll is a set of integers representing the dice roll.
+    # Returns the set of all possible dice to keep.
+    # Output type is a set of sets of tuples of integers.
     def possible_dice_to_keep(self, roll):
+        if len(roll) == 0:
+            return {}
         
+        possibilities = {}
+        for combo in POINT_SCORING_COMBINATIONS.keys():
+            if set(combo) in roll:
+                sub_possibilities = self.possible_dice_to_keep_rec(roll - set(combo)) # Set of sets of tuples
+
+                if len(sub_possibilities) == 0:
+                    possibilities.add({combo})
+                else:
+                    for sub_combo in sub_possibilities.keys():
+                        possibilities.add({combo} + sub_combo) # sub_combo is a set of tuples
+
+        return possibilities # Set of sets of tuples
+        
+    def pick_dice_to_keep(self, roll):
+        raise NotImplementedError("Subclasses must implement this method")
