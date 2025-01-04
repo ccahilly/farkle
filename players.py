294159a496e25f83f0c5bbf77b54270c9093cbcd
Caroline Cahilly <caroline.cahilly@gmail.com>
2025-01-04 12:25:45 -0700

    progress on player class

--- players.py
+++ players.py
@@ -8,8 +8,20 @@ class Player:
         self.dice_kept = 0
     
     def take_turn(self):
-        raise NotImplementedError("Subclasses must implement this method")
-    
+        roll = self.roll_dice()
+        self.turn_score = 0
+
+        while not self.farkle(roll):
+            dice_to_keep = self.pick_dice_to_keep(roll)
+            for dice in dice_to_keep:
+                self.dice_kept -= len(dice)
+                self.turn_score += POINT_SCORING_COMBINATIONS[dice]
+            
+            if self.roll_again(roll):
+                roll = self.roll_dice()
+            else:
+                break
+
     def roll_dice(self):
         roll = {}
         
@@ -22,6 +34,14 @@ class Player:
         
         return roll
     
+    # Output type is a set of tuples of integers and the associated points
+    def pick_dice_to_keep(self, roll):
+        raise NotImplementedError("Subclasses must implement this method")
+    
+    # Output is true or false
+    def roll_again(self, roll):
+        raise NotImplementedError("Subclasses must implement this method")
+
     # Roll is a set of integers representing the dice roll.
     # Returns the set of all possible dice to keep.
     # Output type is a set of sets of tuples of integers.
@@ -41,6 +61,6 @@ class Player:
                         possibilities.add({combo} + sub_combo) # sub_combo is a set of tuples
 
         return possibilities # Set of sets of tuples
-        
-    def pick_dice_to_keep(self, roll):
-        raise NotImplementedError("Subclasses must implement this method")
+    
+    def farkle(self, roll):
+        return len(self.possible_dice_to_keep(roll)) == 0
