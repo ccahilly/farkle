e1fdb7539005c61b4b51baa6f7a73bb084216fa8
Caroline Cahilly <caroline.cahilly@gmail.com>
2025-01-07 17:39:06 -0800

    progress on ev agent

--- players/expectedValuePlayer.py
+++ players/expectedValuePlayer.py
@@ -17,10 +17,10 @@ class ExpectedValuePlayer(Player):
         pass
 
     def fill_rolls_to_actions(self):
-        if DEBUG:
-            print("Filling rolls to actions")
-            print(f"Rolls to actions: {self.rolls_to_actions}")
-            print(f"Length of rolls to actions: {len(self.rolls_to_actions)}")
+        # if DEBUG:
+        #     print("Filling rolls to actions")
+        #     print(f"Rolls to actions: {self.rolls_to_actions}")
+        #     print(f"Length of rolls to actions: {len(self.rolls_to_actions)}")
             
         expected_values_in_terms_of_r6 = np.array([[0,1], 
                                                    [np.nan, np.nan], 
@@ -34,12 +34,12 @@ class ExpectedValuePlayer(Player):
             self.get_expected_value_rec(num_dice, expected_values_in_terms_of_r6)
             print(f"Expected value for {num_dice} dice in terms of r6: {expected_values_in_terms_of_r6[num_dice]}")
 
-        if DEBUG:
-            for roll, action in self.rolls_to_actions.items():
-                points = 0
-                for dice in action:
-                    points += POINT_SCORING_COMBINATIONS[dice]
-                print(f"Roll: {roll} Action: {action} Points: {points}")
+        # if DEBUG:
+        #     for roll, action in self.rolls_to_actions.items():
+        #         points = 0
+        #         for dice in action:
+        #             points += POINT_SCORING_COMBINATIONS[dice]
+        #         print(f"Roll: {roll} Action: {action} Points: {points}")
                 
     def get_expected_value_rec(self, num_dice, evs):
         if DEBUG:
@@ -51,9 +51,10 @@ class ExpectedValuePlayer(Player):
         ev = np.zeros(2)
         new_rolls = dict()
         total_rolls = 0
+        num_farkles = 0
         for roll in self.rolls_to_actions:
-            if DEBUG:
-                print(f"Roll from self.rolls_to_actions: {roll}")
+            # if DEBUG:
+            #     print(f"Roll from self.rolls_to_actions: {roll}")
 
             if len(roll) == num_dice - 1:
                 for i in range(1, 7):
@@ -76,6 +77,7 @@ class ExpectedValuePlayer(Player):
 
                     if len(possible_plays) == 0:
                         new_rolls[new_roll] = () # No move in the case of a farkle
+                        num_farkles += unique_orderings(new_roll)
                     else:
                         max_raw_points = np.argmax(play_values, axis=0)[0]
                         max_r_indices = np.where(play_values[:, 1] == np.max(play_values[:, 1]))[0]
@@ -86,13 +88,16 @@ class ExpectedValuePlayer(Player):
                             # raise ValueError(f"Best play not clear without knowing the value of r6: {new_roll} {play_values} {possible_plays}")
                             print("Best play not clear without knowing the value of r6")
                             print(f"Roll: {new_roll}")
-                            print(f"Play values: {play_values}")
+                            # print(f"Play values: {play_values}")
                             print(f"Possible plays: {possible_plays}")
                             ev += play_values[max_raw_points]
                             new_rolls[new_roll] = possible_plays[max_raw_points]
 
         if DEBUG:
-            print(f"Expected value counts: {ev}")
+            print(f"Probability of farkle: {num_farkles / total_rolls}")
+
+        # if DEBUG:
+        #     print(f"Expected value counts: {ev}")
         
         if total_rolls != 6 ** num_dice:
             raise ValueError(f"Expected value calculation error: {total_rolls} {6 ** num_dice}")
